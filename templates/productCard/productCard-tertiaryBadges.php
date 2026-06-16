39404cbf95199303da4680cb769c15dc071a212d
Jiří Motejzík <motejzik@praguebest.cz>
2026-06-16 12:49:49 +0200

    product badges customizing

--- templates/productCard/productCard-tertiaryBadges.php
+++ templates/productCard/productCard-tertiaryBadges.php
@@ -1,7 +1,13 @@
 <?php if (rand(0,1)) { ?>
     <div class="ProductCard-tertiaryBadges">
-        <div class="Badge Badge--rectangleSide">Novinka</div>
-        <div class="Badge Badge--rectangleSide">Doprodej</div>
-        <div class="Badge Badge--rectangleSide">Sleva</div>
+        <?php if (rand(0,1)) { ?>
+            <div class="Badge Badge--rectangle Badge--primary">Novinka</div>
+        <?php } ?>
+        <?php if (rand(0,1)) { ?>
+            <div class="Badge Badge--rectangle Badge--secondary">Doprodej</div>
+        <?php } ?>
+        <?php if (rand(0,1)) { ?>
+            <div class="Badge Badge--rectangle Badge--tertiary">Sleva -18%</div>
+        <?php } ?>
     </div>
 <?php } ?>
